986fa3d86db14850b0b0acbe487730f63f4620b7
Maurício Hanika <github.com@zurv.de>
2012-04-04 17:29:31 +0200

    Fixing render method parameters

--- Zurv/View/Adapter/FileView.php
+++ Zurv/View/Adapter/FileView.php
@@ -11,25 +11,23 @@ use \Zurv\Response;
  * @author mau
  */
 class FileView implements Adapter {
-	protected $_template = '';
-	
-	public function __construct($file) {
-		if(! file_exists($file)) {
-			throw new \InvalidArgumentException("Could not load view {$file}");
-		}
-		
-		$this->_template = $file;
-	}
-	
-	public function render(array $vars, Request $request, Response $response) {
-		header('Content-Type: text/html; charset="utf8"');
-		
-		ob_start();
-		extract($vars);
-		include $this->_template;
-		$render = ob_get_contents();
-		ob_end_clean();
-		
-		return $render;
-	}
+  protected $_template = '';
+  
+  public function __construct($file) {
+    if(! file_exists($file)) {
+      throw new \InvalidArgumentException("Could not load view {$file}");
+    }
+    
+    $this->_template = $file;
+  }
+  
+  public function render(array $vars) {
+    ob_start();
+    extract($vars);
+    include $this->_template;
+    $render = ob_get_contents();
+    ob_end_clean();
+    
+    return $render;
+  }
 }
