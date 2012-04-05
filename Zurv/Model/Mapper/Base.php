4ee6b05628d0652e226bf528fcaa88a6387d7bc0
Maurício Hanika <github.com@zurv.de>
2012-04-06 00:39:49 +0200

    Hotfix for exception throwing

--- Zurv/Model/Mapper/Base.php
+++ Zurv/Model/Mapper/Base.php
@@ -19,10 +19,10 @@ abstract class Base implements Mapper {
   }
 
 	public function findById($id) {
-		throw new Exception('To be implemented');
+		throw new \Exception('To be implemented');
 	}
 	
 	public function findByAttribute($attribute, $value) {
-		throw new Exception('To be implemented');
+		throw new \Exception('To be implemented');
 	}
 }
